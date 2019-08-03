b688b86c50bd6db8646b0838a72edff9276bc198
pingfei <pingfei1224@163.com>
2019-08-03 18:13:02 +0800

    draw img

--- src/pages/share/canvas-share/share.tsx
+++ src/pages/share/canvas-share/share.tsx
@@ -1,17 +1,26 @@
-import Taro , { Component } from '@tarojs/taro';
-import { View, Text , Button} from '@tarojs/components';
+import Taro , { Component, Config } from '@tarojs/taro';
+import { View, Text , Button, Canvas, Image } from '@tarojs/components';
 import './share.scss'
+import unsplash from '../../../assets/images/unsplash.jpg'
+
 
 export default class CanvasShare extends Component {
 
-   config = {
+   config: Config = {
        navigationBarTitleText: ''
   }
 
   state={}
 
   componentWillMount () {}
-  componentDidMount () {} 
+  componentDidMount () {
+
+    let ctx = Taro.createCanvasContext('canvas', this)
+    console.log(unsplash)
+    ctx.drawImage(unsplash, 0, 0, 345, 300)
+    ctx.stroke()
+    ctx.draw(true)
+  } 
   componentWillReceiveProps (nextProps,nextContext) {} 
   componentWillUnmount () {} 
   componentDidShow () {} 
@@ -21,7 +30,16 @@ export default class CanvasShare extends Component {
   render() {
     return (
       <View>
-        
+        <View className="canvas-box">
+          <Canvas 
+            id="canvas" 
+            canvasId="canvas" 
+            className="canvas" 
+            disableScroll={false}
+            width="345"
+            height="345"
+          ></Canvas>
+        </View>
       </View>
     );
   }
--- src/pages/share/index/index.tsx
+++ src/pages/share/index/index.tsx
@@ -1,12 +1,14 @@
-import Taro , { Component } from '@tarojs/taro';
+import Taro , { Component, Config } from '@tarojs/taro';
 import { View, Text } from '@tarojs/components';
 import './index.scss'
 
 
 export default class ShareIndex extends Component {
 
-   config = {
-       navigationBarTitleText: '分享'
+   config: Config = {
+       navigationBarTitleText: '分享',
+       navigationBarTextStyle: 'white',
+       navigationBarBackgroundColor: '#eee'
   }
 
   state={}
